9f6967fa6df9e5d8bb8de008711960022cd100ec
Kent Georgeson <kgeorgeson@kgeorgeson-a01.vmware.com>
2019-11-15 10:29:40 -0600

    disable tap to zoom

--- planner.js
+++ planner.js
@@ -343,3 +343,12 @@ $(document).ready(function() {
 
     calcTotals();
 })
+
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
